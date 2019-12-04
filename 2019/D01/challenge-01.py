a8c98e4296a017a55d93c6bd3baa50274956a5a0
Sam Roberts <samroberts707@gmail.com>
2019-12-04 11:24:37 +0000

    Code golf challenge 1 for day 1

--- 2019/D01/challenge-01.py
+++ 2019/D01/challenge-01.py
@@ -26,19 +26,12 @@ What is the sum of the fuel requirements for all of the modules on your spacecra
 import math
 
 def calcFuelForMass(mass):
-    if(isinstance(mass, int) == False):
-        return False;
     return int(math.floor(mass / 3) - 2)
 
 fuelRequired = 0
 
-fileInput = open('input.txt' , "r")
-for val in fileInput:
-    # Convert string to int
-    val = int(val)
-    # Calc fuel needed for component mass
-    newVal = calcFuelForMass(val)
-    # Add fuel to counter
-    fuelRequired = fuelRequired + newVal
+with open('input.txt') as _file:
+    for val in _file:
+        fuelRequired += calcFuelForMass(int(val))
     
 print "Fuel needed: ", fuelRequired
