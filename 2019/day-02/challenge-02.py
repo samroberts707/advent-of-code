d1f1914cdafb7dcd59d16db4347aa0c22848c364
Sam Roberts <samroberts707@gmail.com>
2019-12-13 15:15:18 +0000

    Complete Day 2 Part 2

--- 2019/day-02/challenge-02.py
+++ 2019/day-02/challenge-02.py
@@ -12,13 +12,7 @@ def runProgram(programArray):
             elif programArray[operator] == 2:
                 programArray[changingIndex] = programArray[parameterOne] * programArray[parameterTwo]
     return programArray[0]
-
-# Prepare Array
-with open('input.txt') as _file:
-    masterArray = []
-    for var in _file:
-        masterArray.append(int(var))
-
+    
 noun = 0
 verb = 0
 
@@ -27,17 +21,19 @@ while noun <= 99:
     # Start Verb at 0 for each Noun
     verb = 0
     while verb <= 99:
+        # Read file to array
+        with open('input.txt') as _file:
+            masterArray = []
+            for var in _file:
+                masterArray.append(int(var))    
         # set the master array to the current Noun and Verb
         masterArray[1] = noun
         masterArray[2] = verb
         output = runProgram(masterArray)
         if  output == 19690720:
             print('Noun: ', noun, '; Verb: ', verb, '; Output: ', output)
-            print 100 * noun * verb
-            exit
+            print 100 * noun + verb
+            break
         else:
-            print('Noun: ', noun, '; Verb: ', verb, '; Output: ', output)
             verb += 1
     noun += 1
-
-
