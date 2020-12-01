0d666149e78e7fca17c96700a29ad0e680b08205
Sam Roberts <sam@nucreative.co.uk>
2020-12-01 15:06:51 +0000

    Clean up challenge two

--- 2020/day-01/challenge-02.py
+++ 2020/day-01/challenge-02.py
@@ -3,18 +3,10 @@ with open ('input.txt', 'r') as _file:
 
 for firstVal in entries:
     # Subtract value from 2020 and check if the remainder is in the array
-    if 2020 - firstVal in entries:
-        answerOne = firstVal * (2020 - firstVal)
-        break
-
-# Answer One
-print("Two entries that add to 2020 are:", firstVal, ' and ', 2020 - firstVal, '. Multiplied = ', answerOne)
-
-for firstVal in entries:
-    # Subtract value from 2020 and check if the remainder is in the array
     for secondVal in entries:
         if (2020 - firstVal - secondVal) in entries:
             answerTwo = firstVal * secondVal * (2020 - firstVal - secondVal)
             break
 
+# Answer Two
 print("Three entries that add to 2020 are:", firstVal, ', ', secondVal, ' and ', 2020 - firstVal, '. Multiplied = ', answerTwo)
