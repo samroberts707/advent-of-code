1c61264b7e244de4b7794545e9431bc3b9ae8963
Sam Roberts <samroberts707@gmail.com>
2020-12-11 15:54:14 +0000

    Complete Day 06

--- 2020/day-06/challenge-02.py
+++ 2020/day-06/challenge-02.py
@@ -1,10 +1,16 @@
 total_yeses = 0
-
 with open ('input.txt', 'r') as _file:
     surveys = _file.read().split('\n\n')
     for survey in surveys:
-        survey = survey.replace('\n', '')
-        survey = "".join(set(survey))
-        total_yeses = total_yeses + len(survey)
+        survey = survey.split('\n')
+        survey_answer_count = len(survey)
+        for char in survey[0]:
+            letter_count = 0
+            for answer in survey:
+                if char in answer:
+                    letter_count = letter_count + 1
+            if letter_count == survey_answer_count:
+                total_yeses = total_yeses + 1
+
 
 print 'Total unique yeses: ' + str(total_yeses)
