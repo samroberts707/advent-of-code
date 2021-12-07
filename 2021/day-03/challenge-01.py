c0ff24696cfc92143b7b4c6adaf3b0db9cfce7dc
Sam Robets <samroberts707@gmail.com>
2021-12-07 09:17:49 +0000

    Refactor Day 03 Challenge 01 code

--- 2021/day-03/challenge-01.py
+++ 2021/day-03/challenge-01.py
@@ -3,32 +3,26 @@ from collections import Counter
 with open ('input.txt', 'r') as _file:
     entries = [entry.rstrip() for entry in _file]
 
-def gammaRateCalc(binaryList):
-  binaryOutput = []
-  for index in range(len(binaryList[0])):
-    indexCounter = Counter(item[index] for item in binaryList)
-    if indexCounter["0"] > indexCounter["1"]:
-      binaryOutput.append("0")
-    else:
-      binaryOutput.append("1")
-  binaryOutput = ''.join(binaryOutput)
-  return binaryOutput
-
-def epsilonRateCalc(binaryList):
-  binaryOutput = []
+def rateCalc(binaryList):
+  binaryOutput = {
+    'gammaRate': [],
+    'epsilonRate': []
+  }
   for index in range(len(binaryList[0])):
     indexCounter = Counter(item[index] for item in binaryList)
     if indexCounter["0"] < indexCounter["1"]:
-      binaryOutput.append("0")
+      binaryOutput['gammaRate'].append("0")
+      binaryOutput['epsilonRate'].append("1")
     else:
-      binaryOutput.append("1")
-  binaryOutput = ''.join(binaryOutput)
+      binaryOutput['gammaRate'].append("1")
+      binaryOutput['epsilonRate'].append("0")
+  binaryOutput['gammaRate'] = int(''.join(binaryOutput['gammaRate']), 2)
+  binaryOutput['epsilonRate'] = int(''.join(binaryOutput['epsilonRate']), 2)
   return binaryOutput
 
 
-gammaRate = int(gammaRateCalc(entries), 2)
-epsilonRate = int(epsilonRateCalc(entries), 2)
+rates = rateCalc(entries)
 
-print("Gamma rate: ", gammaRate)
-print("Epsilon rate: ", epsilonRate)
-print("Power consumption: ", gammaRate*epsilonRate)
+print("Gamma rate: ", rates['gammaRate'])
+print("Epsilon rate: ", rates['epsilonRate'])
+print("Power consumption: ", rates['gammaRate']*rates['epsilonRate'])
